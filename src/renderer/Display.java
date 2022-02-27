275c8fd184604c225c9c876934b114337fed6534
phicks22 <65268541+phicks22@users.noreply.github.com>
2022-02-26 19:00:15 -0800

    Added display and run methods.

--- src/renderer/Display.java
+++ src/renderer/Display.java
@@ -1,10 +1,28 @@
 package renderer;
 
-import javax.swing.JFrame;
+import java.awt.image.BufferStrategy;
 
-public class Display {
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class Display extends Canvas implements Runnable {
+    private static final long serialVersionUID = 1L;
+
+    private JFrame frame;
+    private static String title = "My first Java program";
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
 
     public Display() {
+        this.frame = new JFrame();
+
+        Dimension size = new Dimension(WIDTH, HEIGHT);
+        this.setPreferredSize(size);
+
+    }
+
+    @Override
+    public void run() {
 
     }
 }
