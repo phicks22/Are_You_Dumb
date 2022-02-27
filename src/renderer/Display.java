d3f42a617a8bc21d3e418d4cb39dbf58ded1c21b
phicks22 <65268541+phicks22@users.noreply.github.com>
2022-02-26 19:06:35 -0800

    Added main and start methods.

--- src/renderer/Display.java
+++ src/renderer/Display.java
@@ -8,17 +8,38 @@ import java.awt.*;
 public class Display extends Canvas implements Runnable {
     private static final long serialVersionUID = 1L;
 
+    private Thread thread;
     private JFrame frame;
     private static String title = "My first Java program";
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
+    public static boolean running = false;
+
 
     public Display() {
         this.frame = new JFrame();
 
         Dimension size = new Dimension(WIDTH, HEIGHT);
         this.setPreferredSize(size);
+    }
 
+    public static void main(String[] args) {
+        Display display = new Display();
+        display.frame.setTitle(title);
+        display.frame.add(display);
+        display.frame.pack();
+        display.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        display.frame.setLocationRelativeTo(null);
+        display.frame.setResizable(false);
+        display.frame.setVisible(true);
+
+        display.start();
+    }
+
+    public synchronized void start(){
+        running = true;
+        this.thread = new Thread(this, "Display");
+        this.thread.start();
     }
 
     @Override
